5f8fe482e57701c3ddb3ef116eca44db1218f077
KrovavyiParen <hiroazora@gmail.com>
2025-02-28 21:29:32 +0500

    add some fansy but still unworking

--- Components/Services/NoteService.cs
+++ Components/Services/NoteService.cs
@@ -5,23 +5,25 @@ namespace VSHCTwebApp.Components.Services
 {
     public class NoteService
     {
-        private static readonly List<Note> notes = new List<Note>();
-        private readonly List<Note> _notes = notes;
+        private readonly List<Note> _notes = new List<Note>();
 
         public void AddNote(Note note)
         {
             if (note == null)
+            {
                 throw new ArgumentNullException(nameof(note));
+            }
 
             note.CreatedAt = DateTime.Now;
-            notes.Add(note);
+            _notes.Add(note);
+            Console.WriteLine($"Записей в сервисе: {_notes.Count}");
         }
 
         public List<Note> GetNotes() => _notes;
 
         public Note GetNoteByTitle(string title)
         {
-            return notes.FirstOrDefault(n => n.Title == title);
+            return _notes.FirstOrDefault(n => n.Title == title);
         }
     }
 }
