adf911fa66509dec4193285c63c2c8389f700e11
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2025-03-17 22:25:27 +0500

    Roles

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI;
 using VSHCTwebApp.Components;
 using VSHCTwebApp.Components.Account;
 using VSHCTwebApp.Components.Services;
@@ -28,16 +29,20 @@ namespace VSHCTwebApp
             builder.Services.AddScoped<IdentityRedirectManager>();
             builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
+            
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+            });
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDbContextFactory<VSHCTwebAppContext>(options =>
@@ -45,6 +50,7 @@ namespace VSHCTwebApp
                     builder.Configuration.GetConnectionString("VSHCTwebAppContext") ??
                     throw new InvalidOperationException(
             "Connection string 'VSHCTwebAppContext' not found.")));
+
 
             builder.Services.AddDbContextFactory<VSHCTwebAppContext>(options =>
                 options.UseSqlServer(
